544ba273ba11c9272851c119df90a037db391ddc
perjo927 <perjo927@student.liu.se>
2018-04-01 19:34:22 +0200

    feat(cv-sections): support click

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import './index.scss'
+import "./index.scss";
 
-console.log("hello world 2");
+// TODO: tests
+
+const sections = Array.from(
+  ["skills", "work", "education", "recommendations"],
+  x => document.getElementsByClassName(x)
+);
+
+const toggleShow = event => {
+  const parent = event.target.className;
+  const content = document.querySelectorAll(`.${parent} .content`)[0];
+  console.log("parent", parent, "content", content)
+  content.classList.toggle("show");
+};
+
+sections.forEach((section, i, a) => {
+  for (let s of section) {
+    s.onclick = toggleShow;
+  }
+});
