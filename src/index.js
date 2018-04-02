3e0b539b3560cea3f3f364316aa1918f77e8168a
perjo927 <perjo927@student.liu.se>
2018-04-02 17:58:04 +0200

    feat(cv-sections): finish feature

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import "./index.scss";
 
 // TODO: tests
 
+const openDoor = event => event.target.classList.add("close");
 
-const toggleShow = (event) => {
-  const parent = event.target.parentElement.className;
-  const content = document.querySelectorAll(`.${parent} .content`)[0];
+const toggleShow = event => {
+  const parent = event.target.parentElement;
+  const content = parent.querySelectorAll(`.content`)[0];
   content.classList.toggle("show");
 };
 
 const sections = document.getElementsByClassName("toggle");
+const door = document.getElementsByClassName("door")[0];
+door.onclick = openDoor;
 
 for (let s of sections) {
-    s.onclick = toggleShow;
-  }
+  s.onclick = toggleShow;
+}
